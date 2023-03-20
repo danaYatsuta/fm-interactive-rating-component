5877c19b722775aef7090a45424d0a3a2a58feb9
Dana Yatsuta <danayatsuta@gmail.com>
2023-03-20 04:26:34 +0300

    Setup custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      'orange': 'hsl(25, 97%, 53%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'light-grey': 'hsl(217, 12%, 63%)',
+      'medium-grey': 'hsl(216, 12%, 54%)',
+      'dark-blue': 'hsl(213, 19%, 18%)',
+      'very-dark-blue': 'hsl(216, 12%, 8%)'
+    },
+    fontFamily: {
+      'overpass': ['Overpass', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
